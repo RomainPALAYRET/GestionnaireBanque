ca3a7baa73d3c878daa66deecf84097754044432
Lethlos <romain.rp.palayert@gmail.com>
2023-03-31 15:52:10 +0200

    Interface : creation de 2 Objets TextField et un Label

--- src/main/java/com/example/gestionnairebanque/Controleur.java
+++ src/main/java/com/example/gestionnairebanque/Controleur.java
@@ -1,14 +1,35 @@
 package com.example.gestionnairebanque;
 
+import com.example.gestionnairebanque.models.GestionnaireBancaire;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class Controleur {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controleur implements Initializable {
+
+    private GestionnaireBancaire gest;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField nomField;
+
+    @FXML
+    private TextField montantField;
+
+    @FXML
+    private Label soldeLabel;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            gest = new GestionnaireBancaire();
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
