152733f7d660a6e759f6d11546378bc5c423940f
Lethlos <romain.rp.palayert@gmail.com>
2023-03-31 14:57:34 +0200

    Création par génération de la classe GestionnaireBancaire

---
+++ src/main/java/com/example/gestionnairebanque/models/GestionnaireBancaire.java
@@ -0,0 +1,40 @@
+/*
+ * Application Gestionnaire d'un compte bancaire                                                              31/03/2023
+ * TP Java Avancé : TP noté Gestion d'un compte bancaire
+ * 3il Ingenieur Rodez
+ */
+package com.example.gestionnairebanque.models;
+
+import java.util.List;
+
+/**
+ * Classe principale du modèle, possédant :
+ *                                   - un solde
+ *                                   - une liste des transactions effectuées
+ *                                   - une liste des taux à appliquer
+ * Cette classe a été développée en TDD
+ * @author Romain PALAYRET
+ * @version 1.0.0
+ */
+public class GestionnaireBancaire {
+
+    List<Transaction> transactions;
+    List<Taux> listTaux;
+    double solde;
+
+    /**
+     * Récupère une liste des transactions depuis un fichier binaire
+     * Récupère la liste des taux depuis le fichier taux.txt
+     * A la création du compte, le solde est de 0.00€
+     */
+    public GestionnaireBancaire() {
+    }
+
+    /**
+     * @return le solde du compte
+     */
+    public double getSolde() {
+        return solde;
+    }
+
+}
