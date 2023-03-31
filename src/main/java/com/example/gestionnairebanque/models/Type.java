ee3d27acda8a6d462b836b56b5ddcd281da9ec1f
Lethlos <romain.rp.palayert@gmail.com>
2023-03-31 14:05:22 +0200

    Ajout d'une énumération type pour les Transaction

---
+++ src/main/java/com/example/gestionnairebanque/models/Type.java
@@ -0,0 +1,16 @@
+/*
+ * Application Gestionnaire d'un compte bancaire                                                              31/03/2023
+ * TP Java Avancé : TP noté Gestion d'un compte bancaire
+ * 3il Ingenieur Rodez
+ */
+package com.example.gestionnairebanque.models;
+
+/**
+ * Les différents types de transactions possibles
+ * @author Romain PALAYRET
+ * @version 1.0.0
+ */
+public enum Type {
+    CREDIT, DEBIT
+}
+
