8a3c3f9fca4a23604efde7950bbf91177e64e644
Lethlos <romain.rp.palayert@gmail.com>
2023-03-31 15:01:20 +0200

    TDD GestionnaireBancaire : test 1 échec

---
+++ src/main/java/com/example/gestionnairebanque/models/tests/GestionnaireBancaireTest.java
@@ -0,0 +1,26 @@
+/*
+ * Application Gestionnaire d'un compte bancaire                                                              31/03/2023
+ * TP Java Avancé : TP noté Gestion d'un compte bancaire
+ * 3il Ingenieur Rodez
+ */
+package com.example.gestionnairebanque.models.tests;
+
+
+import com.example.gestionnairebanque.models.GestionnaireBancaire;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+/**
+ * Test Junit de la classe GestionnaireBancaire
+ * @author Romain PALAYRET
+ * @version 1.0.0
+ */
+public class GestionnaireBancaireTest {
+    @Test
+    public void testConstructeur() {
+        GestionnaireBancaire gest = new GestionnaireBancaire();
+        assertEquals(gest.getSolde(), 0.00, 0.01);
+    }
+}
